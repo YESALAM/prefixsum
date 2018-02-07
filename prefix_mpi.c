b4d2aa429ae939c065be7d1cb9b8b4b304df36d2
yesalam <itisalam@gmail.com>
2018-02-07 16:20:14 +0530

    mpi lagbhag final

--- prefix_mpi.c
+++ prefix_mpi.c
@@ -6,6 +6,38 @@
 
 #define LIST_SIZE  8000000
 
+int* prefix_sum_chunk(int *list,int size){
+	int log_of_chunk_size  = ceil(log(size)/log(2));	
+	int *temp = (int *)malloc(sizeof(int) * chunk_size);	
+	for(int i=0;i<log_of_chunk_size;i++){	
+		int* current ;
+		int* prev ;
+		if(i%2==0) {
+			current = temp ;
+			prev  = list ;
+		}else{
+			current = list;
+			prev = temp;
+		}	
+		for(int j=0;j<chunk_size;j++){
+			int poi = pow(2,i);			
+			if(j<poi){
+				current[j] = prev[j];
+			}else{
+				current[j] = prev[j]+prev[j-poi];
+			}
+		}
+	}
+	if((log_of_chunk_size-1)%2 == 0) return temp ;
+	else return list ;
+}
+
+void add_last_toAll(int *list,int size,int last_item){
+	for(int i=0;i<size;i++){
+		list[i] = list[i]+last_item ;
+	}
+}
+
 int main(int argc,char *argv[]){
 	int rank;
 	int size;
@@ -18,33 +50,67 @@ int main(int argc,char *argv[]){
 	MPI_Comm_rank(&rank);
 	MPI_Comm_size(&size);
 	
+	int chunk_size = LIST_SIZE  / size ;
+
 	int * list = NULL ;
 	int *temp = NULL ;
 	if(rank == 0 ){
 		list = (int *) malloc(sizeof(int) * LIST_SIZE );
-		int chunk_size = LIST_SIZE  / size ;
+		for(int i=0;i<LIST_SIZE;i++){
+			list[i] = i+1;
+		}
+		
 		for(int i=1;i<size;i++){
 			int *sub_list = list+chunk_size ;
-			MPI_Send(list,chunk_size,MPI_INT,i,0,MPI_COMM_WORLD);			
+			if(i==size-1) MPI_Send(sub_list,chunk_size+(LIST_SIZE%size),MPI_INT,i,0,MPI_COMM_WORLD);
+			else MPI_Send(sub_list,chunk_size,MPI_INT,i,0,MPI_COMM_WORLD);			
 		}
-		int log_of_chunk_size  = ceil(log(size)/log(2));
-		for(int i=0;i<log_of_chunk_size;i++){
-			
-			for(int j=0;j<chunk_size;j++){
-				int poi = pow(2,i);
-				temp = (int *)malloc(sizeof(int) * chunk_size*2);
-				int f_index;
-				if(i%2==0) f_index = 1;
-				else f_index = 0;
-				if(j<poi){
-					temp[f_index][j] = temp[f_index^1][j];	
-				}else{
-					temp[f_index][j] = temp[f_index^1][j] + temp[j-poi];
-				}
+
+
+		temp = prefix_sum_chunk(list,chunk_size);
+		
+		int last_item = &temp[chunk_size-1];
+		MPI_Send(&last_item,1,MPI_INT,1,1,MPI_COMM_WORLD);
+		
+		int final_sum ;
+		MPI_STATUS status2;
+		MPI_Recv(&final_sum,1,MPI_INT,size-1,1,MPI_COMM_WORLD,&status2);
+
+		int *result = (int* ) malloc(sizeof(int)*LIST_SIZE);
+		int pointer = 0;
+		for(int i=0;i<chunk_size;i++){
+			result[i]=temp[i];
+		}
+
+		for(int =1;i<size;i++){			
+			if(i == size-1) chunk_size = chunk_size + (LIST_SIZE%size) ;
+			MPI_STATUS status;
+			int *temp = (int*) malloc(sizeof(int)*chunk_size);
+			MPI_Recv(temp,chunk_size,MPI_INT,i,2,MPI_COMM_WORLD,&status);
+			for(int j=0 ;j<chunk_size;j++){
+				result[j] = temp[pointer+j];
 			}
+			pointer = chunk_size ;			
 		}
-		int *last_item = &temp[0][chunk_size-1];
-		MPI_Send(last_item,1,MPI_INT,1,1,MPI_COMM_WORLD);
+
+	}else{
+				
+		if(rank == size-1) chunk_size = chunk_size + (LIST_SIZE%size) ;
+		temp = (int*) malloc(sizeof(int) * chunk_size);
+		MPI_STATUS status;
+		MPI_Recv(temp,chunk_size,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+		temp = prefix_sum_chunk(temp,chunk_size);
+		int pre_last_item ;
+		MPI_STATUS last_status;
+		MPI_Recv(&pre_last_item,1,MPI_INT,rank-1,1,MPI_COMM_WORLD,&last_status);
+		add_last_toAll(temp,chunk_size,pre_last_item);
+		int last_item = temp[0][chunk_size-1];
+		if(rank != size-1) MPI_Send(&last_item,1,MPI_INT,rank+1,1,MPI_COMM_WORLD);
+		else MPI_Send(&last_item,1,MPI_INT,0,1,MPI_COMM_WORLD);
+		
+		MPI_Send(&temp,chunk_size,MPI_INT,0,2,MPI_COMM_WORLD);
 
 	}
+
+	
 }
